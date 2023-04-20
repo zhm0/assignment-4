20457f29302f1ba06d70b9360a7c2388defdb304
Mike Zhang <zhangmike0@gmail.com>
2023-04-20 03:55:12 -0400

    Created and tested credits page

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,15 +5,30 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
+import ListView from './ListView';
+import AddForm from './AddForm';
 
 const Credits = (props) => {
+  // Render the list of Credit items and a form to input new Credit item
   return (
-    <div>
-      <h1>Credits</h1>
+    <div className='landing-page'>
+      <h1 style={{textDecoration: 'underline'}}>Credits</h1>
+      <h2>Credit History</h2>
+      <ListView list={props.credits}/>
+      <br/>
+      <br/>
+      <AccountBalance accountBalance={props.accountBalance}/>
+      <br/>
+      <h2>Add Credit</h2>
+      <AddForm addItem={props.addCredit}/>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
   );
 }
+
+
+
 
 export default Credits;
